82b1be5e20b7159939bb0367a3504b7761b49815
Anthony Simonov <asimonov@engee.cloud>
2025-09-04 16:38:52 +0300

    fix

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-from pydantic import AnyUrl, Field
+from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
@@ -7,7 +7,7 @@ class Config(BaseSettings):
         description="Токен для тг бота",
         default="8152863814:AAE_efaQn_0B3spZtcY2dIgEx3Mb07wiaoY",
     )
-    OLLAMA_URL: AnyUrl = Field(default="http://localhost:11434")
+    OLLAMA_URL: str = Field(default="http://localhost:11434")
     MAX_FILE_SIZE_DOWNLOAD: int = Field(
         default=128 * 1024**2, description="Ограничение по загрузке в трекер в байтах"
     )
@@ -21,6 +21,7 @@ class Config(BaseSettings):
     )
     LLM: str = Field(description="Используемая llm", default="llama3")
     API_PORT: int = Field(description="Порт сервера uvicorn")
+    DATABASE_URL: str = Field()
 
 
 config = Config()  # type: ignore
