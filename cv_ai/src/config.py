9c12905595d418257961cd693d72edacbf03eb53
brmstr <brmstr4me@gmail.com>
2025-09-05 11:41:27 +0300

    asdad

--- cv_ai/src/config.py
+++ cv_ai/src/config.py
@@ -1,7 +1,6 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 class Config(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
@@ -10,35 +9,25 @@ class Config(BaseSettings):
         extra="ignore",
     )
 
-    # Заменяем на DeepSeek-V2-Lite модель с Hugging Face
+    # Базовая модель DeepSeek (Hugging Face)
     BASE_MODEL: str = Field(
-        default="deepseek-ai/DeepSeek-V2-Lite",  # Указываем модель DeepSeek
+        default="Qwen/Qwen1.5-4B-Chat",
         description="Базовая модель для DeepSeek"
     )
-
-    SYSTEM_PROMPT: str = Field(
-        default=(
-            "Сравни вакансию и резюме и дай только одно число, которое будет означать процент соответствия между ними. "
-            "Это число должно быть в пределах от 0 до 100. "
-            "Не выводи никаких дополнительных символов, текста или описаний, только число. "
-            "Пример правильного вывода: 85. "
-            "Никаких других фраз или списков не должно быть."
-        ),
-        description="Системные инструкции для модели",
-    )
-
-    DATASET_PATH: str = Field(
-        description="Путь до датасета", default="./dataset/cv.json"
+    # Путь до датасета (пример)
+    PATH_TO_DATASET: str = Field(
+        description="Путь до датасета",
+        default="./dataset/cv.json"
     )
     ENV: str = Field(description="Окружение: dev, prod", default="dev")
+
     PATH_TO_TEST_VACANCY: str = Field(
         description="Путь до тестовой вакансии",
-        default="C:\\Users\\user\\source\\repos\\ai_hr\\ai_hr\\cv_ai\\src\\dataset\\вакансия.txt"
+        default="dataset/вакансия.txt"
     )
     PATH_TO_TEST_RESUME: str = Field(
         description="Путь до тестового резюме",
-        default="C:\\Users\\user\\source\\repos\\ai_hr\\ai_hr\\cv_ai\\src\\dataset\\резюме.txt"
+        default="dataset/резюме.txt"
     )
-
 
 config = Config()
