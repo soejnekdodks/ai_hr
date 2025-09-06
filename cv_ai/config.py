aa7ea32a06c7653d34f54743311b57f8651c65a2
brmstr <brmstr4me@gmail.com>
2025-09-06 19:24:59 +0300

    конфиг

--- cv_ai/config.py
+++ cv_ai/config.py
@@ -1,11 +1,15 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 class Config(BaseSettings):
-    BASE_MODEL: str = Field(
-        default="Vikhrmodels/Vikhr-7B-instruct_0.4", description="Базовая модель"
-    )
 
+    # Vikhrmodels/Vikhr-7B-instruct_0.4 если потяжелее
+    # Vikhrmodels/Vikhr-Qwen-2.5-0.5b-Instruct легкая пиздец
+    # Vikhrmodels/QVikhr-3-4B-Instruction
+
+    BASE_MODEL: str = Field(
+        default="Vikhrmodels/Vikhr-Llama-3.2-1B-Instruct",
+        description="Базовая модель"
+    )
 
 config = Config()
