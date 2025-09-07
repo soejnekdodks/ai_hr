89701ad84f6f5d7cc31950a1aaa6f0fd9f34d5fd
Anthony Simonov <asimonov@engee.cloud>
2025-09-07 08:18:01 +0300

    -mock api

--- app/presentation/api.py
+++ app/presentation/api.py
@@ -15,26 +15,6 @@ from app.presentation.models import AnswersRequest, Question, QuestionsResponse
 router = APIRouter()
 
 
-@router.post("/api/v1/create-mock")
-async def create_mock(session: AsyncSession = Depends(get_async_session)) -> str:
-    candidate = await query.candidate.create(session, b"\x00" + os.urandom(4) + b"\x00")
-    alias_id = uuid.uuid4()
-    await query.interview.create(
-        session,
-        candidate.id,
-        [
-            "Что такое замыкание (closure) в JavaScript?",
-            "Как избежать Callback Hell?",
-            "Объясните принципы REST.",
-            "Что такое миграции базы данных и зачем они нужны?",
-            "Как вы обеспечиваете безопасность своего API?",
-        ],
-        alias_id,
-    )
-    await session.commit()
-    return f"http://91.209.135.81/api/v1/deeplink?id={alias_id}"
-
-
 @router.get("/api/v1/questions", response_model=QuestionsResponse)
 async def get_questions(
     interview_id: uuid.UUID, session: AsyncSession = Depends(get_async_session)
@@ -62,7 +42,7 @@ async def post_answers(
 
     if interview is None:
         raise HTTPException(status_code=404, detail="Interview not found")
-    
+
     if interview.state != InterviewState.OPEN:
         raise HTTPException(
             status_code=406, detail="Interview saving does not acceptable"
@@ -77,7 +57,7 @@ async def post_answers(
     # Анализируем ответы кандидата
     questions = [q.question for q in interview.questions]
     answers = [answer.answer for answer in data.answers]
-    
+
     analyzer = AnswersAnalyzer()
     report = analyzer.analyze_answers(questions, answers)
 
@@ -88,7 +68,7 @@ async def post_answers(
     await query.questions.set_answers(
         session, [answer.model_dump() for answer in data.answers]
     )
-    
+
     # Закрываем интервью
     await query.interview.mark_as_finished(session, interview.id)
 
@@ -98,8 +78,3 @@ async def post_answers(
 @router.get("/api/v1/deeplink")
 async def deeplink(id: str) -> None:
     return RedirectResponse(url=f"vtbhackaton://interview/{id}")
-
-
-@router.get("/api/v1/all-questions")
-async def get_all_questions(session: AsyncSession = Depends(get_async_session)):
-    return await query.questions.all(session)
