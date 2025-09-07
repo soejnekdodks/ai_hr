295005f0d033b3e92c054c4fd2c0ae470a7acc1b
Anthony Simonov <asimonov@engee.cloud>
2025-09-07 16:41:50 +0300

    raf format

---
+++ app/database/core.py
@@ -0,0 +1,46 @@
+from typing import Any, AsyncGenerator
+
+from pydantic import BaseModel
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.pool import NullPool
+
+from app.config import config
+
+
+class Base(DeclarativeBase):
+    def as_dict(self, *, exclude: set[str] | None = None) -> dict[str, Any]:
+        exclude_columns = set() if exclude is None else exclude
+        columns_data: dict[str, Any] = {}
+        for column in self.__table__.columns:
+            if column.name in exclude_columns:
+                continue
+
+            value = getattr(self, column.name)
+
+            if isinstance(value, BaseModel):
+                columns_data[column.name] = value.dict()
+            elif isinstance(value, list) and all(
+                isinstance(item, BaseModel) for item in value
+            ):
+                columns_data[column.name] = [item.dict() for item in value]
+            else:
+                columns_data[column.name] = value
+        return columns_data
+
+
+engine = create_async_engine(
+    config.POSTGRES_URL, poolclass=NullPool, pool_pre_ping=True
+)
+async_session = async_sessionmaker(
+    class_=AsyncSession,
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        async with session.begin():
+            yield session
